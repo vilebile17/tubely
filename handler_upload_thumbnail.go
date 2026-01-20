f0bceffd8d1d054e73e8b7f1a7a59e507d7081c0
Ibrahim Somauroo <ibrahimsomauroo@proton.me>
2026-01-20 09:04:01 +0000

    Added a way to add thumbnails to videos

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,48 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	if err = r.ParseMultipartForm(maxMemory); err != nil {
+		respondWithError(w, http.StatusBadRequest, "There was an error parsing the request", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "There was an error forming the file", err)
+		return
+	}
+	mediaType := header.Header.Get("Content-Type")
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "There was an error reading the data", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "There was an error retrieving the video data from the database", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You can't upload the thumbnail to a video that you don't own...", nil)
+		return
+	}
+
+	videoThumbnails[video.ID] = thumbnail{
+		data:      data,
+		mediaType: mediaType,
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%v", cfg.port, videoID.String())
+	video.ThumbnailURL = &thumbnailURL
+	if err = cfg.db.UpdateVideo(video); err != nil {
+		respondWithError(w, http.StatusBadRequest, "There was an error adding the video data from the database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
